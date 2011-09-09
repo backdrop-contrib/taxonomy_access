f391ab17d8707e9a23a77d9943d0e92e51557e28
xjm <xjm@65776.no-reply.drupal.org>
2011-09-09 07:58:52 -0500

    by xjm: Try to make JS more readable by using ksenzee's pattern.

--- tac_create.js
+++ tac_create.js
@@ -1,17 +1,18 @@
-// Using the closure to map jQuery to $. 
-(function ($) {
-  // Store our function as a property of Drupal.behaviors.
-  Drupal.behaviors.tac_create = {
-    attach: function(context, settings) {
-      var $fields = $(Drupal.settings.taxonomy_access);
-      
-      $.each($fields, function(i, field) {
-          var fieldname = "." + field.field;
-          $.each(field.dis, function(j, tid) {
-              selector = fieldname + " [value='" + tid + "']";
-              $(selector).attr('disabled','disabled');
-            });
-        });
-    }
-  }
-}(jQuery));
+/**
+ * Disable disallowed terms in taxonomy fields.
+ *
+ * @see http://jsdemystified.drupalgardens.com/
+ */
+Drupal.behaviors.tac_create = {};
+Drupal.behaviors.tac_create.attach = function(context, settings) {
+  var $ = jQuery;
+  var $fields = $(Drupal.settings.taxonomy_access);
+
+  $.each($fields, function(i, field) {
+    var fieldname = "." + field.field;
+    $.each(field.disallowed_tids, function(j, tid) {
+      selector = fieldname + " [value='" + tid + "']";
+      $(selector).attr('disabled','disabled');
+    });
+  });
+}
